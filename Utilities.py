d48e9e4cc439bdbebe0211e9303700be725de601
Owais-Khan <owais.khan@alumni.utoronto.ca>
2022-05-05 12:25:18 -0400

    blah

--- Utilities.py
+++ Utilities.py
@@ -1,114 +1,257 @@
-import dolfin as df
+import vtk
+from vmtk import vtkvmtk, vmtkscripts
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 import numpy as np
-import argparse
 from glob import glob
-import h5py
 
-mpi_comm = df.MPI.comm_world
-my_rank = df.MPI.rank(mpi_comm)
+############ Read Dicom Folder ############
+def ReadDicomFiles(FolderName):
+	FileList=sorted(glob("%s/*.dcm"%FolderName))
+	print (FileList[0])
+	Image=vmtkscripts.vmtkImageReader()
+	print (dir(Image))
+	exit(1)
+	Image.InputFileName(FileList[0])
+	Image.ImageOutputFileName("/Users/mokhan/GoogleDrive/Owais/Research_Postdoc/perfusion_project/Simvascular/CABG1A/Images/abc.vti")
+	Image.Update()
+
+#ReadDicomFiles("/Users/mokhan/GoogleDrive/Owais/Research_Postdoc/perfusion_project/Simvascular/CABG1A/Images/CTA")
+
+	
+	
+
+############ Input/Output ##################
+def ReadVTUFile(FileName):
+	reader=vtk.vtkXMLUnstructuredGridReader()
+	reader.SetFileName(FileName)
+	reader.Update()
+	return reader.GetOutput()
+
+def ReadVTKFile(FileName):
+	reader = vtk.vtkStructuredPointsReader()
+	reader.SetFileName(FileName)
+	reader.ReadAllVectorsOn()
+	reader.ReadAllScalarsOn()
+	reader.Update()
+	return reader.GetOutput()
+
+def ReadVTPFile(FileName):
+	reader=vtk.vtkXMLPolyDataReader()
+	reader.SetFileName(FileName)
+	reader.Update()
+	return reader.GetOutput()
+
+def ReadVTIFile(FileName):
+	reader = vtk.vtkXMLImageDataReader() 
+	reader.SetFileName(FileName) 
+	reader.Update()
+	return reader.GetOutput()
+
+def WriteVTUFile(FileName,Data):
+	writer=vtk.vtkXMLUnstructuredGridWriter()
+	writer.SetFileName(FileName)
+	writer.SetInputData(Data)
+	writer.Update()
+        
+def WriteVTPFile(FileName,Data):
+	writer=vtk.vtkXMLPolyDataWriter()
+	writer.SetFileName(FileName)
+	writer.SetInputData(Data)
+	writer.Update()
 
 
-class Utilities():
-	def __init__(self,Args):
-		self.Args=Args
+############# Mesh Morphing Functions ###############
+        #Create a line from apex and centroid of the myocardium
+        
+def CreateLine(Point1,Point2,Length):
+	line0=np.array([Point1[0]-Point2[0],Point1[1]-Point2[1],Point1[2]-Point2[2]])
+	line1=-1*line0
+	line0=(line0/np.linalg.norm(line0))*(Length/2.)
+	line1=(line1/np.linalg.norm(line1))*(Length/2.)
+	return line0,line1
 
-	def Main(self):
-		#Convert h5 to vtu
-		if self.Args.ConvertHDF5ToVTU==True:
-			#Loop over all files if no start/stop/increment provided
-			if self.Args.StartTimestep is None:
-				FileNames_=sorted(glob(self.Args.InputFolder+"/*up.h5"))
-				self.Args.StartTimestep=int(FileNames_[0].split("=")[1].split("_")[0])
-				self.Args.StopTimestep=int(FileNames_[-1].split("=")[1].split("_")[0])
-				self.Args.Increment=int(FileNames_[1].split("=")[1].split("_")[0])-self.Args.StartTimestep
-				print ("--- The Start Timestep is: %d"%self.Args.StartTimestep)
-				print ("--- The End Timestep is:   %d"%self.Args.StopTimestep)
-				print ("--- The Increment is:      %d"%self.Args.Increment)
-				
-			print ("--- Converting Velocity from HDF5 to VTU format")
-			self.ConvertHDF5ToVTU()
-		
-		if self.Args.ProjectVelocityToNewMesh==True:
-			print ("--- Projecting Velocity on New Mesh")
-			self.ProjectVelocityToNewMesh()
+def CreatePolyLine(Coords):
+	# Create a vtkPoints object and store the points in it
+	points = vtk.vtkPoints()
+	for i in range(len(Coords)): points.InsertNextPoint(Coords[i])
+
+	#Create a Polyline
+	polyLine = vtk.vtkPolyLine()     
+	polyLine.GetPointIds().SetNumberOfIds(len(Coords))
+	for i in range(len(Coords)): polyLine.GetPointIds().SetId(i, i)
+
+	# Create a cell array to store the lines in and add the lines to it
+	cells = vtk.vtkCellArray()
+	cells.InsertNextCell(polyLine)
+
+	# Create a polydata to store everything in
+	polyData = vtk.vtkPolyData()
+    
+	# Add the points to the dataset
+	polyData.SetPoints(points)
+
+	# Add the lines to the dataset
+	polyData.SetLines(cells)
+	
+	return polyData 
+
+def ClosestPoint(Point, Array):
+	dist_2 = np.sum((Array - Point)**2, axis=1)
+	return Array[np.argmin(dist_2)],np.argmin(dist_2)
+
+def FurthestPoint(Point, Array):
+        dist_2 = np.sum((Array - Point)**2, axis=1)
+        return Array[np.argmax(dist_2)],np.argmax(dist_2)
+
+        
+def CutPlane(Volume,Origin,Norm):
+	plane=vtk.vtkPlane()
+	plane.SetOrigin(Origin)
+	plane.SetNormal(Norm)
+	Slice=vtk.vtkCutter()
+	Slice.GenerateTrianglesOff()
+	Slice.SetCutFunction(plane)
+	Slice.SetInputData(Volume)
+	Slice.Update()
+	return Slice.GetOutput()
+
+def CutLine(Slice,Point,Centroid,Norm1):
+	#Get the two in-plane normals
+	Norm2_slice=(Point-Centroid)/np.linalg.norm(Point-Centroid)
+	Norm3_slice=np.cross(Norm1,Norm2_slice)
+	
+	#Generate the two planes
+	plane_N2=vtk.vtkPlane()
+	plane_N2.SetOrigin(Centroid)
+	plane_N2.SetNormal(Norm2_slice)
+	plane_N3=vtk.vtkPlane()
+	plane_N3.SetOrigin(Centroid)
+	plane_N3.SetNormal(Norm3_slice)
+	
+	#Clip the plane to get a line across the diameter
+	Line =vtk.vtkCutter()
+	Line.GenerateTrianglesOff()
+	Line.SetCutFunction(plane_N3)
+	Line.SetInputData(Slice)
+	Line.Update()
+        
+	#Separate the line into only one quarter (i.e. half the line)
+	Line1=vtk.vtkClipPolyData()
+	Line1.SetClipFunction(plane_N2)
+	Line1.SetInputData(Line.GetOutput())
+	Line1.Update()
+	return Line1.GetOutput()
+
+def GetCentroid(Surface):
+	Centroid=vtk.vtkCenterOfMass()
+	Centroid.SetInputData(Surface)
+	Centroid.SetUseScalarsAsWeights(False)
+	Centroid.Update()
+	return Centroid.GetCenter()
+
+def GetSurfaceArea(Surface):
+	masser=vtk.vtkMassProperties()
+	masser.SetInputData(Surface)
+	masser.Update()
+	return masser.GetSurfaceArea()
+
+def ExtractSurface(volume):
+	#Get the outer surface of the volume
+	surface=vtk.vtkDataSetSurfaceFilter()
+	surface.SetInputData(volume)
+	surface.Update()
+	return surface.GetOutput()
+        
+#Print the progress of the loop
+def PrintProgress(self,i,N,progress_old):
+	progress_=(int((float(i)/N*100+0.5)))
+	if progress_%10==0 and progress_%10!=progress_old: print ("    Progress: %d%%"%progress_)
+	return progress_%10
+
+def TagOuterSurface(Surface):
+	#Create an OBB tree and cast Rays       
+	obbTree = vtk.vtkOBBTree()
+	obbTree.SetDataSet(Surface)
+	obbTree.BuildLocator()
+	pointsVTKintersection = vtk.vtkPoints()
+
+	#Create an array to store surface tags
+	Surface_tags=np.zeros(Surface.GetNumberOfPoints())
+       
+	#Get Centroid
+	Centroid=np.array(GetCentroid(Surface))
+
+	#Loop over all the points. 
+	for i in range(Surface.GetNumberOfPoints()):
+		pSource=np.array(Surface.GetPoint(i))
+		pTarget=pSource+np.array((pSource-Centroid))*5
+		code = obbTree.IntersectWithLine(pSource, pTarget, pointsVTKintersection, None)
+		X=pointsVTKintersection.GetData().GetNumberOfTuples()
+		if X>1: Surface_tags[i]=1
 			
-	
-	#This function will convert HDF5 file format to VTU File Format
-	def ConvertHDF5ToVTU(self):
-		#Load the Mesh
-		print ("------ Loading Mesh %s"%self.Args.MeshFileName)
-		Mesh=df.Mesh(self.Args.MeshFileName)
-		
-		#Create a Function Space
-		V=df.VectorFunctionSpace(Mesh,"CG",self.Args.VelocityOrder)
-		u=df.Function(V)
-		Npts=u.vector().size()/3
+	#Store the data in Surface array
+	#Tags for out or inner surface
+	Surface_tags_vtk=numpy_to_vtk(Surface_tags,deep=True)
+	Surface_tags_vtk.SetName("Tags")
+	Surface.GetPointData().AddArray(Surface_tags_vtk)
+	Surface.Modified()
+                
+	return Surface
 
-		#Loop over the time steps
-		for i in range(self.Args.StartTimestep,self.Args.StopTimestep+self.Args.Increment,self.Args.Increment):
-			FileName_=glob(self.Args.InputFolder+"%.05d*.h5")[0]
-			print ("------ Looping over: %s"%FileName_)
-			Velocity_=df.HDF5File(Mesh.MPI.comm_world,FileName_, "r")
-			Velocity_.read(u,"u")		
+#Smooth Surface
+def SurfaceSmoothing(Surface,Nits,PB_value,method="Taubin"):
+	if method=="Taubin":
+		smoothingFilter = vtk.vtkWindowedSincPolyDataFilter()
+		smoothingFilter.SetInputData(Surface)
+		smoothingFilter.SetNumberOfIterations(Nits)
+		smoothingFilter.SetPassBand(PB_value)
+		smoothingFilter.SetBoundarySmoothing(True)
+		smoothingFilter.Update()
+		return smoothingFilter.GetOutput()
+	elif method=="Laplace":
+		smoothingFilter = vtk.vtkSmoothPolyDataFilter()
+		smoothingFilter.SetInputData(Surface)
+		smoothingFilter.SetNumberOfIterations(Nits)
+		smoothingFilter.SetRelaxationFactor(PB_value)
+		smoothingFilter.Update()
+		return smoothingFilter.GetOutput()
+	else:
+		print ("Error. The smoothing filter was not found")
+		exit(1)
 
+def SurfaceAddArray(Surface,Array,ArrayName):
+	SurfaceArray=numpy_to_vtk(Array,deep=True)
+	SurfaceArray.SetName(ArrayName)
+	Surface.GetPointData().AddArray(SurfaceArray)
+	Surface.Modified()
+	return Surface
 
-	def ProjectVelocityToNewMesh(self):
-		#Load the Reference Mesh
-		print ("------ Loading Mesh: %s"%self.Args.MeshFileName)
-		Mesh=df.Mesh(self.Args.MeshFileName)
+def ProjectedPointOnLine(coord_,Centroid,Apex,Norm1):
+	#Find the location (coord,distance) on the LV Apex-Base axis
+	dist_P_to_line_=np.sqrt(vtk.vtkLine.DistanceToLine(coord_,Centroid,Apex))
+	dist_P_to_Apex_=np.power( np.power(coord_[0]-Apex[0],2) + np.power(coord_[1]-Apex[1],2) + np.power(coord_[2]-Apex[2],2),0.5)
+	dist_Apex_to_ProjP_=np.power(np.power(dist_P_to_Apex_,2)-np.power(dist_P_to_line_,2),0.5)
+	coord_ProjP_=Apex-Norm1*dist_Apex_to_ProjP_
 
-		#Load New Mesh
-		VV=VectorFunctionSpace(mesh,"CG",self.uOrder)
-		u=Function(VV)
-		N=u.vector().size()/3	
-		
+	return coord_ProjP_
 
+def SurfaceNormals(Surface):
+	normals = vtk.vtkPolyDataNormals()
+	normals.SetInputData(Surface)
+	normals.SetFeatureAngle(90)
+	normals.AutoOrientNormalsOn()
+	#  normals.GetOutput().ReleaseDataFlagOn()
+	normals.UpdateInformation()
+	normals.Update()
+	Surface = normals.GetOutput()
 
+	return Surface
 
+def ThresholdByUpper(Volume,arrayname,value):
+	Threshold=vtk.vtkThreshold()
+	Threshold.SetInputData(Volume)
+	Threshold.ThresholdByUpper(value)
+	Threshold.SetInputArrayToProcess(0,0,0,"vtkDataObject::FIELD_ASSOCIATION_POINTS",arrayname)
+	Threshold.Update()
+	return Threshold.GetOutput()
 
-
-
-if __name__=="__main__":
-        #Description
-	parser = argparse.ArgumentParser(description="This is a general Utilitiy script to perform several taks related to Oasis solver, pre-processing and post-processing.")
-	
-	parser.add_argument('-MeshFileName', '--MeshFileName', type=str,required=True, dest="MeshFileName",help="The path/to/filename where ")
-	
-	parser.add_argument('-VelocityOrder', '--VelocityOrder', type=int,required=True, dest="VelocityOrder",help="The polynomial order for the velocity function space.")
-	
-	parser.add_argument('-InputFolder', '--InputFolder', type=str, required=True, dest="InputFolder", help="The folder that containts the velocity data")
-
-
-#---------------------------- Convert HDF5 Files to VTU Files ------------        
-	parser.add_argument('-ConvertHDF5ToVTU', '--ConvertHDF5ToVTU', type=bool, required=False, default=False, dest="ConvertHDF5ToVTU",help="Convert velocity from H5 format to Vtu format")
-	
-	parser.add_argument('-StartTimestep', '--StartTimestep', type=int, required=False, dest="StartTimestep",help="Starting timestep to process the data")
-	parser.add_argument('-StopTimestep', '--StopTimestep', type=int, required=False, dest="StopTimestep",help="Last timestep to process the data")
-	parser.add_argument('-Increment', '--Increment', type=int, required=False, dest="Increment",help="Timestep increment")
-
-
-
-
-
-#----------------------------- ProjectVelocityToNewMesh------------------
-
-
-	#parser.add_argument('-ProjectVelocityToNewMesh', '--ProjectVelocityToNewMesh', type=bool, required=False, default=True, dest="ProjectVelocityToNewMesh",help="Project the Velocity and Pressure from one mesh to another mesh.")
-
-	#parser.add_argument('-MeshFileName', '--MeshFileName', type=str,required=False, dest="MeshFileName",help="The path/to/filename where ")
-
-	#parser.add_argument('-NewMeshFileName', '--NewMeshFileName', type=str, required=False, dest="NewMeshFileName", help="Related to ProjectVelocityToNewMesh. Filename for the new mesh on which to project the velocity data")
-	
-	#parser.add_argument('-OutFolder', '--OutFolder', type=str, required=False, dest="OutFolder", help="The output folder to store all the projected velocity data")
-
-#------------------------------- Velocity Data -------------------------
-	#parser.add_argument('-VelocityFolder', '--VelocityFolder', type=str, required=False, dest="VelocityFolder", help="The folder where velocity data is stored")
-
-	#parser.add_argument('-StartTimeStep', '--StartTimeStep', type=int, required=False, dest="StartTimeStep", help="The starting time step to be processed")
-	
-	#parser.add_argument('-EndTimeStep', '--EndTimeStep', type=int, required=False, dest="EndTimeStep", help="The end time step to be processed")
-	
-	#parser.add_argument('-Increment', '--Increment', type=int, required=False, dest="Increment", help="Timestep increment")
-
-	args=parser.parse_args()
-	Utilities(args).Main()
