c1d6b3879d528329c32922dca48b54da6782dcbe
Owais-Khan <owais.khan@alumni.utoronto.ca>
2022-06-06 14:35:59 -0400

    removed backflow

--- OasisMeshWriterForSimVascular.py
+++ OasisMeshWriterForSimVascular.py
@@ -98,8 +98,6 @@ class OasisMeshWriterForSimVascular():
 		AreaRatio=np.array(OutletSurfaceArea)/np.sum(OutletSurfaceArea)
 		self.IDs["area_ratio"]=np.ndarray.tolist(AreaRatio)
 
-		self.IDs["BackFlowFactor"]=self.Args.BackFlowFactor	
-	
 		print ("--- Writing the Boundary IDS to: %s"%self.Args.OutputFileName+"_info.json")
 		with open(self.Args.OutputFileName+"_info.json", 'w') as convert_file:
 			convert_file.write(json.dumps(self.IDs))
@@ -178,11 +176,7 @@ if __name__=="__main__":
 	parser.add_argument('-InputFolder', '--InputFolder', type=str, required=True, dest="InputFolder",help="The path to mesh-complete folder from SimVascular")
 <<<<<<< HEAD
 	parser.add_argument('-ScalingFactor', '--ScalingFactor', type=int, required=False,help="Scale the mesh from cm to mm.")
-=======
 
-	parser.add_argument('-BackFlowFactor', '--BackFlowFactor', type=float, required=False, default=0., dest="BackFlowFactor",help="Backflow stabalization factor to avoid the simulation from diverging in case of flow reversal. Default is 0, meaning no stabalization. A value of 0.2 is typically sufficient.")
->>>>>>> dd248a2e0287442bcd3145eab46d314d44cf8175
-        
 	#Output Filename 
 	parser.add_argument('-OutputFileName', '--OutputFileName', type=str, required=False, dest="OutputFileName",help="The output file in which to store the dolfin mesh.")
 
