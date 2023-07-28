0a75d3e0ff2112527ea5dfdf13d55f910d04bd55
AnahitaSeresti <aseresti@torontomu.ca>
2023-07-28 17:08:25 -0400

    update

--- AnalyticalSolution.py
+++ AnalyticalSolution.py
@@ -7,7 +7,7 @@ class AnalyticalSolution():
 	def __init__(self,args):
 		self.Args = args
 	def main(self):
-		Mesh = UnitIntervalMesh(self.Args.MeshSize)
+		Mesh = UnitIntervalMesh(self.Args.NofElements)
 		c1 = Constant(self.Args.ConstantVal1)
 		c2 = Constant(self.Args.ConstantVal2)
 		V = Constant(self.Args.MeanVelocity)
@@ -22,7 +22,7 @@ class AnalyticalSolution():
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "This script calculates concentration based on the 1D analytical solution for the steady advection diffusion based on what was presented in Arzani's work")
-	parser.add_argument('-MeshSize', '--MeshSize', type=int, required=False, default = 100, dest = "MeshSize")
+	parser.add_argument('-NofElements', '--NofElements', type=int, required=False, default = 100, dest = "NofElements")
 	parser.add_argument('-ConstantVal1', '--ConstantVal1', type=float, required=False, default = 1.0, dest = "ConstantVal1")
 	parser.add_argument('-ConstantVal2', '--ConstantVal2', type=float, required=False, default = 0.1, dest = "ConstantVal2")
 	parser.add_argument('-MeanVelocity', '--MeanVelocity', type=float, required=False, default = 50.0, dest = "MeanVelocity")
